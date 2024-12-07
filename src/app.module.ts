8c26e22915d3a0c17653aa346378a4788e72a807
Ahmed-Sami-Abdelaleem <ahmed222333sami@gmail.com>
2024-12-07 20:55:34 +0200

    intial schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module'; // Import the Prisma module
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule, // Add the Prisma module
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
